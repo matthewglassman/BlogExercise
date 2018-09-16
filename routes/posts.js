65ce7a231d87383bf58b1a0c8314c8ceef9f5125
matthewglassman <matthewglassman78@gmail.com>
2018-09-16 18:06:59 -0400

    Starting to add functionality to routes involving posts

--- routes/posts.js
+++ routes/posts.js
@@ -6,11 +6,14 @@ app.use(bodyParser.json())
 
 module.exports = {
   app.getPosts('/posts', (req, res) => {
-
+    res.status(200).send(store.posts)
   })
 
   app.addPost('/posts', (req, res) =>{
-
+    let newPost = req.body
+    let id = store.posts.length
+    store.posts.push(newPost)
+    res.status(201).send({id: id})
   })
 
   app.updatePost('/posts/:postId', (req, res) => {
@@ -18,6 +21,6 @@ module.exports = {
   })
 
   app.deletePost('/posts/:postId', (req, res) => {
-    
+
   })
 }
