c99c883f129ab784a089516e3c647f20620cac50
matthewglassman <matthewglassman78@gmail.com>
2018-10-06 13:50:00 -0400

    Coding Blog Post API

--- routes/posts.js
+++ routes/posts.js
@@ -17,10 +17,13 @@ module.exports = {
   })
 
   app.updatePost('/posts/:postId', (req, res) => {
-
+    store.posts[req.params.id] = req.body
+    res.status(200).send(store.posts[req.params.id])
   })
 
+//Must come back and code in deletion of relative comments as well.
   app.deletePost('/posts/:postId', (req, res) => {
-
+    store.posts.splice(req.params.id, 1)
+    res.status(204).send()
   })
 }
